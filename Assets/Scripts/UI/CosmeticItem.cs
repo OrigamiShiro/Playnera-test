e28371327511c5702e9cbe03054082139c64e11d
Stanislav <76973214+OrigamiShiro@users.noreply.github.com>
2026-03-25 18:44:08 +0300

    feat: add CosmeticItem and CosmeticContainer

---
+++ Assets/Scripts/UI/CosmeticItem.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using MakeupMechanic.Data;
+
+namespace MakeupMechanic.UI
+{
+    public class CosmeticItem : MonoBehaviour, ICosmetic, IPointerClickHandler
+    {
+        private CosmeticItemSO _data;
+
+        public CosmeticItemSO Data => _data;
+        public event Action<ICosmetic> OnClick;
+
+        public void Init(CosmeticItemSO data)
+        {
+            _data = data;
+            GetComponent<Image>().sprite = data.itemSprite;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnClick?.Invoke(this);
+        }
+    }
+}
